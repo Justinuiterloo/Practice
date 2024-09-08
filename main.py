f31c554c44b2d49d63f7f9f3771fc3ce6a4d81fa
12204838 <justinapps15@gmail.com>
2024-09-08 14:33:40 +0200

    remove redundant print statements

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from person import Person
 
-# Creating an instance of the Person class
 justin: Person  = Person("Justin", 16, 1.80)
-
-# Accessing attributes of the justin instance
-print(justin.name)
-print(justin.age)
-print(justin.height)
-
 justin.setName("Fleur")
-
-print(justin.getName())
